9637b9a3cb40773eb9478bdeb0109f3ade9a3ccf
MJDA7 <mj.deandaabrego@yahoo.com.mx>
2020-06-15 21:03:08 -0500

    Carpeta componentes y Navbar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const axios = require ("axios");
+
 function App() {
   return ( <>
+    const axios = require ("axios");
+     
   </>
     
   );
