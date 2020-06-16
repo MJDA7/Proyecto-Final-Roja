f4056f332b78f500134bf182d5fc1edce021cdc4
gabamio <gbamio99@gmail.com>
2020-06-16 04:43:48 +0200

    creacion de componente navbar

---
+++ src/Componentes/Navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ''
+import './App.css';
+
+const axios = require ("axios");
+
+function Navbar() {
+  return ( <>
+  
+     
+  </>
+    
+  );
+}
+
+export default Navbar;
